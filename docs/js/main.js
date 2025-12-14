37b81948e8d8b78abe8765295211107d325e1e83
Artyushenko Daniil <foxones80@gmail.com>
2025-12-14 19:28:09 +0600

    added constructor

--- docs/js/main.js
+++ docs/js/main.js
@@ -7569,21 +7569,23 @@ document.addEventListener("DOMContentLoaded", () => {
 __webpack_require__.r(__webpack_exports__);
 const header = document.querySelector(".header");
 if (header) {
-  const nav = header.querySelector(".nav");
-  if (nav) {
-    const navWithChildren = nav.querySelectorAll(".menu-item-has-children");
-    navWithChildren.forEach(item => {
-      const subMenu = item.querySelector(".sub-menu");
-      if (subMenu) {
-        item.addEventListener("mouseenter", e => {
-          item.classList.add("active");
-          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-        });
-        item.addEventListener("mouseleave", e => {
-          item.classList.remove("active");
-          subMenu.style.maxHeight = null;
-        });
-      }
+  const navigations = header.querySelectorAll(".nav");
+  if (navigations.length) {
+    navigations.forEach(nav => {
+      const navWithChildren = nav.querySelectorAll(".menu-item-has-children");
+      navWithChildren.forEach(item => {
+        const subMenu = item.querySelector(".sub-menu");
+        if (subMenu) {
+          item.addEventListener("mouseenter", e => {
+            item.classList.add("active");
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+          });
+          item.addEventListener("mouseleave", e => {
+            item.classList.remove("active");
+            subMenu.style.maxHeight = null;
+          });
+        }
+      });
     });
   }
 }
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,24 +1,26 @@
 const header = document.querySelector(".header");
 
 if (header) {
-  const nav = header.querySelector(".nav");
+  const navigations = header.querySelectorAll(".nav");
 
-  if (nav) {
-    const navWithChildren = nav.querySelectorAll(".menu-item-has-children");
+  if (navigations.length) {
+    navigations.forEach((nav) => {
+      const navWithChildren = nav.querySelectorAll(".menu-item-has-children");
 
-    navWithChildren.forEach((item) => {
-      const subMenu = item.querySelector(".sub-menu");
+      navWithChildren.forEach((item) => {
+        const subMenu = item.querySelector(".sub-menu");
 
-      if (subMenu) {
-        item.addEventListener("mouseenter", (e) => {
-          item.classList.add("active");
-          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-        });
-        item.addEventListener("mouseleave", (e) => {
-          item.classList.remove("active");
-          subMenu.style.maxHeight = null;
-        });
-      }
+        if (subMenu) {
+          item.addEventListener("mouseenter", (e) => {
+            item.classList.add("active");
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+          });
+          item.addEventListener("mouseleave", (e) => {
+            item.classList.remove("active");
+            subMenu.style.maxHeight = null;
+          });
+        }
+      });
     });
   }
 }
